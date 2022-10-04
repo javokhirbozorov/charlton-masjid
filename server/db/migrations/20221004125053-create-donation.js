67f8c133cd71e4a24b09725e16393e75ce22fb26
Javokhir <javokhir.bozorov@gmail.com>
2022-10-04 13:54:59 +0100

    models

---
+++ server/db/migrations/20221004125053-create-donation.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Donations', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      body: {
+        type: Sequelize.TEXT
+      },
+      imgLink: {
+        type: Sequelize.TEXT
+      },
+      goal: {
+        type: Sequelize.INTEGER
+      },
+      total: {
+        type: Sequelize.INTEGER
+      },
+      adminId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Donations');
+  }
+};
---
+++ server/db/models/donation.js
@@ -0,0 +1,28 @@
+const {
+  Model,
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Donation extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Donation.init({
+    title: DataTypes.STRING,
+    body: DataTypes.TEXT,
+    imgLink: DataTypes.TEXT,
+    goal: DataTypes.INTEGER,
+    total: DataTypes.INTEGER,
+    adminId: DataTypes.INTEGER,
+  }, {
+    sequelize,
+    modelName: 'Donation',
+  });
+  return Donation;
+};
