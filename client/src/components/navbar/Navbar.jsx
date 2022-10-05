3ffa796d67d6e5d8e5ac071d88b50a3da6ae124c
Taism Chaniev <taismdg@icloud.com>
2022-10-05 15:56:42 +0200

    footer end

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -41,7 +41,7 @@ function classNames(...classes) {
 export default function Example() {
   return (
     <>
-      <Popover className="fixed w-full bg-white z-50">
+      <Popover className="fixed w-full bg-white z-50 navbar">
         <div className="mx-auto max-w-7xl px-4 sm:px-6">
           <div className="flex items-center justify-between border-b-2 border-gray-100 py-2 md:justify-start md:space-x-10">
             
