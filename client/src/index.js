bc85c5baf4dbcadc7ad05187ea97b7f02da7fef2
Taism Chaniev <taismdg@icloud.com>
2022-10-05 11:08:08 +0200

    auth-registration and login

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Posts from './pages/Posts/Posts';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-        <Posts />
+  <App />
 );
 
-reportWebVitals();
