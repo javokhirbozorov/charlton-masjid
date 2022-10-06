2c9832c191cec35d2e26d97d2323a8010431e9d4
Rashid <rnurshinov@gmail.com>
2022-10-06 19:20:29 +0500

    releaseHome1.1

--- client/src/components/ThreeEventsList/ThreeEventsList.jsx
+++ client/src/components/ThreeEventsList/ThreeEventsList.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const ThreeEventsList = () => {
     const products = useSelector((store) => store.products);
   return (
     <div  className='events text-center'>
-    <div className=''>Events</div>
+    <div>Events</div>
 <div className='eventslist'>
 
 
@@ -18,7 +19,9 @@ const ThreeEventsList = () => {
 : "" )}
 
 </div>
+<Link to="/events">
 <button type="button" class="px-6 py-2.5 bg-green-500 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-green-600 hover:shadow-lg focus:bg-green-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-700 active:shadow-lg transition duration-150 ease-in-out">More</button>
+</Link>
 </div>
   )
 }
