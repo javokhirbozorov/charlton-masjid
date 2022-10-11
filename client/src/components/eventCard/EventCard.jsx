1634a1ae5a253e9828770ed3508fa16faa168f64
Rashid <rnurshinov@gmail.com>
2022-10-11 13:19:07 +0500

    Blyat

--- client/src/components/eventCard/EventCard.jsx
+++ client/src/components/eventCard/EventCard.jsx
@@ -6,7 +6,6 @@ const EventCard = ( { product } ) => {
       <a key={product.id} href={product.href} className="group">
         <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
           <p className="mt-1 text-lg font-medium text-gray-900">{product.title}</p>
-
             <img
               src={product.imgLink}
               alt=''
@@ -16,7 +15,7 @@ const EventCard = ( { product } ) => {
           <h3 className="mt-4 text-sm text-gray-700">{product.title}</h3>
           <p className="mt-1 text-lg font-medium text-gray-900">{product.body}</p>
       </a>
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
         Button
       </button>
     </div>
