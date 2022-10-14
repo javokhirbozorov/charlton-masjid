8da9efe021487a50b77e3a102022925c9a5e2184
Rashid <rnurshinov@gmail.com>
2022-10-14 06:52:40 +0500

    Logout1.1

--- server/src/routes/NewAdminRoute.js
+++ server/src/routes/NewAdminRoute.js
@@ -1,14 +1,25 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 
 const route = express.Router();
 const { Admin } = require('../../db/models');
 
 route.post('/', async (req, res) => {
-  try {
-    await Admin.create(req.body);
-    res.json({ info: 'Пришло!' });
-  } catch (error) {
-    console.log(error);
+  const { username, email, password } = req.body;
+  if (email && username && password) {
+    try {
+      const checkIfEmailExists = await Admin.findOne({ where: { email } });
+      if (checkIfEmailExists) {
+        res.json({ emailExists: 'This mail is already in use' });
+      } else {
+        const hash = await bcrypt.hash(password, 10);
+        const admin = await Admin.create({ email, username, password: hash });
+        console.log(admin);
+        res.json({ info: 'Админ создан!' });
+      }
+    } catch (err) {
+      console.log(err);
+    }
   }
 });
 
